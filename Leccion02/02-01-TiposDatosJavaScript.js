1ddb4250fc50583a86e02903b1b8e2c190c8df28
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-12-10 12:35:59 -0500

    JCMA Tipos de Datos

---
+++ Leccion02/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,20 @@
+/*
+    Tipos de Datos
+*/
+
+// Cadena
+var nombre = "Jean Carlo";
+console.log(nombre)
+
+// Numero
+var numero = 1000;
+console.log(numero)
+
+// Object
+var objeto = {
+    nomber: "Gissela",
+    edad: 27,
+    telefono: "0981336321"
+}
+
+console.log(objeto)
