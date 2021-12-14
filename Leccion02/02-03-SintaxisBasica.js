7a8f4dfad754ba94e06ab9d3e14fcf5d846f6176
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-12-13 21:57:16 -0500

    JCMA Sintaxis Básica

---
+++ Leccion02/02-03-SintaxisBasica.js
@@ -0,0 +1,14 @@
+/*
+    Sintaxis Básica
+*/
+// var ya no se recuendia actualmente
+// let: Para variables
+// const: Para constantes
+
+let nombre
+nombre = 'Juan'
+console.log(nombre)
+
+const apellido= 'Perez'
+//Sintaxis Prohibida
+//apellido = 'Lara'
