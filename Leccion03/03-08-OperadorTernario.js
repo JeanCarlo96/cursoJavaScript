7b9ee74d971cb62893bb685e20eeb67e74cc09fb
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-12-16 18:00:38 -0500

    JCMA Operador Ternario

---
+++ Leccion03/03-08-OperadorTernario.js
@@ -0,0 +1,9 @@
+/* Operador Ternario */
+// condición ? valor verdadero : valor falso
+let resultado = (1 > 2) ? "Verdadero" : "Falso";
+console.log(resultado);
+
+let numero = 9;
+
+resultado = (numero % 2 == 0) ? "Número Par" : "Número Impar";
+console.log(resultado);
