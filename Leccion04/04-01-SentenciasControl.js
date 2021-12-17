632169a2a8a1264e66724fc1c1daf0f6dc14d996
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-12-17 15:38:57 -0500

    JCMA Sentencia If

---
+++ Leccion04/04-01-SentenciasControl.js
@@ -0,0 +1,13 @@
+let condicion = true;
+
+if(2 > 3) {
+    console.log('Condición Verdadera');
+}else{
+    console.log('Condición Falsa');
+}
+
+// SI solo hay una línea de código no es necesario las llaves
+if(condicion)
+    console.log('Condición Verdadera');
+else
+    console.log('Condición Falso');
