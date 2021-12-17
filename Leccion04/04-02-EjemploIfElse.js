c86ace6da6296bb0ba6b79420fd3c4d6b72d8206
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-12-17 15:41:46 -0500

    JCMA Ejemplo If Else

---
+++ Leccion04/04-02-EjemploIfElse.js
@@ -0,0 +1,15 @@
+/* Ejercicio If Else */
+
+let numero = 3;
+
+if(numero == 1){
+    console.log("Número Uno");
+}else if(numero == 2){
+    console.log('Número Dos');
+} else if(numero == 3){
+    console.log('Númeor Tres');
+} else if(numero == 4){
+    console.log('Número Cuatro');
+} else{
+    console.log('Número Desconocido');
+}
