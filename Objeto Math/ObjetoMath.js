6b38f1e8394605c025ae27e7d1f4b456f26c8115
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2022-03-31 11:49:12 -0500

    JCMA Objeto Math

---
+++ Objeto Math/ObjetoMath.js
@@ -0,0 +1,16 @@
+/* Objeto Math */
+// Es un objeto estático
+
+console.log('PI: ' + Math.PI);
+console.log(`Valor Absoluto de un número: ${Math.abs(-8)}` );
+console.log('Rendondeo para arriba: ' + Math.ceil(7.2));
+console.log(`Redondeo para abajo: ${Math.floor(7.8)}`);
+console.log(`Redondeo al más cercano: ${Math.round(7.8)}`);
+console.log(`Redondeo al más cercano: ${Math.round(7.2)}`);
+console.log('Raíz cuadrada: ' + Math.sqrt(81));
+console.log(`Elevar potencias: ${Math.pow(2,5)}`);
+console.log('Indicador, positivo, negativo o cero: ' + Math.sign(-10));
+console.log('Indicador, positivo, negativo o cero: ' + Math.sign(0));
+console.log('Indicador, positivo, negativo o cero: ' + Math.sign(7));
+console.log(`Número Aleatorio entre 0 y 1: ${Math.random()}`);
+console.log(`Número Aleatorio entre 0 y 1000: ${Math.round(Math.random() * 1000)}`);
